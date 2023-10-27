fb34d21e5c269a1657500e2d46cbaa7f57ebbd5c
Juan Pedraza <95057188+nachopedraza1@users.noreply.github.com>
2023-10-27 12:22:51 -0300

    megamenu

---
+++ utils/constants.ts
@@ -0,0 +1,34 @@
+export interface Megamenu {
+    type: string,
+    models: string[]
+}
+
+export const megamenu: Megamenu[] = [
+    {
+        type: 'iPhones',
+        models: [
+            'iPhones XR',
+            'iPhone XS Max',
+            'iPhone XS',
+            'iPhone 11',
+            'iPhone 11 Pro Max',
+            'iPhone 11 Pro',
+            'iPhone 12 mini',
+            'iPhone 12',
+            'iPhone 12 Pro',
+            'iPhone 12 Pro Max',
+            'iPhone 13 mini',
+            'iPhone 13',
+            'iPhone 13 Pro',
+            'iPhone 13 Pro Max',
+            'iPhone 14',
+            'iPhone 14 Plus',
+            'iPhone 14 Pro',
+            'iPhone 14 Pro Max',
+            'iPhone 15',
+            'iPhone 15 Plus',
+            'iPhone 15 Pro',
+            'iPhone 15 Pro Max',
+        ]
+    }
+]
