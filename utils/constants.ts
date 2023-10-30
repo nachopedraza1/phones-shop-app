588e64e268ffd1ab3bbe5c45d35d3cd28ba72804
Juan Pedraza <95057188+nachopedraza1@users.noreply.github.com>
2023-10-30 10:24:32 -0300

    asd

--- utils/constants.ts
+++ utils/constants.ts
@@ -30,5 +30,32 @@ export const megamenu: Megamenu[] = [
             'iPhone 15 Pro',
             'iPhone 15 Pro Max',
         ]
+    },
+    {
+        type: 'iPads',
+        models: [
+            'iPhones XR',
+            'iPhone XS Max',
+            'iPhone XS',
+            'iPhone 11',
+            'iPhone 11 Pro Max',
+            'iPhone 11 Pro',
+            'iPhone 12 mini',
+            'iPhone 12',
+            'iPhone 12 Pro',
+            'iPhone 12 Pro Max',
+            'iPhone 13 mini',
+            'iPhone 13',
+            'iPhone 13 Pro',
+            'iPhone 13 Pro Max',
+            'iPhone 14',
+            'iPhone 14 Plus',
+            'iPhone 14 Pro',
+            'iPhone 14 Pro Max',
+            'iPhone 15',
+            'iPhone 15 Plus',
+            'iPhone 15 Pro',
+            'iPhone 15 Pro Max',
+        ]
     }
 ]
