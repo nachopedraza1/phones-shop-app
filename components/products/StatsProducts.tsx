b2e1bcfd764228451d4354efb0d2d20289929c75
nachopedraza1 <nachi_choco@live.com.ar>
2023-11-05 21:55:42 -0300

    stats products

--- components/products/StatsProducts.tsx
+++ components/products/StatsProducts.tsx
@@ -1,19 +1,49 @@
-import { Attribute } from "@/interfaces/MeliProduct"
+import { Attribute } from "@/interfaces/MeliProduct";
+import TableStats from "@/components/tables/TableStats";
 import { Grid, Typography } from "@mui/material";
 
 const StatsProducts: React.FC<{ attributes: Attribute[] }> = ({ attributes }) => {
-    console.log({ attributes });
 
     const productName = attributes.find(item => item.id === "MODEL")?.value_name;
 
+    const generalAtributes = attributes.filter(atr =>
+        atr.id === 'BRAND' ||
+        atr.id === 'LINE' ||
+        atr.id === 'MODEL' ||
+        atr.id === 'MAIN_COLOR'
+    );
+
+    const memoryAtributes = attributes.filter(atr =>
+        atr.id === 'RAM' ||
+        atr.id === 'MEMORY_CARD_MAX_CAPACITY' || 
+        atr.id === 'INTERNAL_MEMORY' || 
+        atr.id === 'WITH_MEMORY_CARD_SLOT'
+    );
+
+    const procesadorAtributes = attributes.filter(atr =>
+        atr.id === 'PROCESSOR_MODEL' ||
+        atr.id === 'GPU_MODEL'
+    );
+
+    const sistemAtributes = attributes.filter(atr =>
+        atr.id === 'OPERATING_SYSTEM_NAME' ||
+        atr.id === 'GPU_MODEL'
+    );
+
     return (
-        <Grid >
+        <Grid>
             <Typography variant="h5"> Características de {productName} </Typography>
-            {
-                attributes.map(atr => (
-                    <Typography> {productName} </Typography>
-                ))
-            }
+            <Grid container gap={4}>
+                <Grid item xs={4}>
+                    <TableStats attributes={generalAtributes} title='Características generales' />
+                </Grid>
+                <Grid item xs={4}>
+                    <TableStats attributes={memoryAtributes} title='Memoria' />
+                </Grid>
+                <Grid item xs={4}>
+                    <TableStats attributes={procesadorAtributes} title='Procesador' />
+                </Grid>
+            </Grid>
         </Grid>
     )
 }
---
+++ components/tables/TableStats.tsx
@@ -0,0 +1,50 @@
+'use client';
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, tableCellClasses } from '@mui/material';
+import { Attribute } from "@/interfaces/MeliProduct"
+
+const TableStats: React.FC<{ attributes: Attribute[], title: string }> = ({ attributes, title }) => {
+
+    const StyledTableCell = styled(TableCell)(({ theme }) => ({
+        [`&.${tableCellClasses.head}`]: {
+            backgroundColor: theme.palette.common.black,
+            color: theme.palette.common.white,
+        },
+        [`&.${tableCellClasses.body}`]: {
+            fontSize: 14,
+        },
+    }));
+
+    const StyledTableRow = styled(TableRow)(({ theme }) => ({
+        '&:nth-of-type(odd)': {
+            backgroundColor: '#ebebeb',
+        },
+    }));
+
+    return (
+        <TableContainer component={Paper}>
+            <Table>
+                <TableHead>
+                    <TableRow>
+                        <TableCell colSpan={6} sx={{ fontWeight: 600 }}> {title} </TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {attributes.map((row) => (
+                        <StyledTableRow key={row.name}>
+                            <StyledTableCell>
+                                {row.name}
+                            </StyledTableCell>
+                            <StyledTableCell>
+                                {row.value_name}
+                            </StyledTableCell>
+                        </StyledTableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    )
+}
+
+export default TableStats;
